49d238381182d884da2dc1a341013163fea0c326
adamlarue19 <111079858+adamlarue19@users.noreply.github.com>
2023-01-12 10:56:42 -0500

    asdmasd

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const cors = require("cors");
+
 
 
 // server used to send send emails
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(PORT, () => console.log("Server Running"));
